72cf889f09815681ec82c7ded5c2f4b74db06a9d
chaizerv <larri@rambler.ru>
2021-02-14 00:11:26 +0300

    Обновление2

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Articles
 
 
 def index(request):
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.all()
+    return render(request, 'news/news_home.html', {'news': news})
 
