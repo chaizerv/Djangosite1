8fbe6ee62b2432d43964a1e7d0c7a4407500870c
chaizerv <larri@rambler.ru>
2021-02-13 15:53:42 +0300

    Обновление1

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('egrul/', views.egrul, name='egrul'),
     path('egrn/', views.egrn, name='egrn'),
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,31 +2,36 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {'title': 'Сигирия - Юридические услуги',
+            'values': '1',
+            'value': '3',
+
+    }
+    return render(request, 'main/index.html', data)
 
 def home(request):
-    return render(request, 'main/home.html')
+    return render(request, 'main/home.html', {'title': 'О нас'})
 
 def egrul(request):
-    return render(request, 'main/egrul.html')
+    return render(request, 'main/egrul.html', {'title': 'Выписки из ЕГРЮЛ'})
 
 def egrn(request):
-    return render(request, 'main/egrn.html')
+    return render(request, 'main/egrn.html', {'title': 'Выписки из ЕГРН'})
 
 def i_egrul(request):
-    return render(request, 'main/i_egrul.html')
+    return render(request, 'main/i_egrul.html', {'title': 'Внесение изменений в ЕГРЮЛ'})
 
 def document(request):
-    return render(request, 'main/document.html')
+    return render(request, 'main/document.html', {'title': 'Восстановление документов'})
 
 def ip(request):
-    return render(request, 'main/ip.html')
+    return render(request, 'main/ip.html', {'title': 'Регистрация ИП'})
 
 def ooo(request):
-    return render(request, 'main/ooo.html')
+    return render(request, 'main/ooo.html', {'title': 'Регистрация ООО'})
 
 def spec(request):
-    return render(request, 'main/spec.html')
+    return render(request, 'main/spec.html', {'title': 'Специальные предложения'})
 
 def contact(request):
-    return render(request, 'main/contact.html')
+    return render(request, 'main/contact.html', {'title': 'Контакты'})
